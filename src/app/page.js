63e79a474668493a5be2ef263d484bc87a7efac5
Mike Dedys <mdedys@users.noreply.github.com>
2024-05-17 11:49:01 -0400

    Push new code

--- src/app/page.js
+++ src/app/page.js
@@ -11,21 +11,7 @@ export default function Home() {
 
   useEffect(() => {
     LogRocket.init('9u6cay/test-project');
-    Sentry.init({
-      dsn: "https://977762c30d26acf92de0e99f5c6c8197@o4507266865889280.ingest.us.sentry.io/4507266867593216",
-      integrations: [
-        Sentry.browserTracingIntegration(),
-        Sentry.replayIntegration(),
-      ],
-      // Performance Monitoring
-      tracesSampleRate: 1.0, //  Capture 100% of the transactions
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-      // Session Replay
-      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-    });
-  }, []) 
+  }, [])
 
   return (
     <main className={styles.main}>
